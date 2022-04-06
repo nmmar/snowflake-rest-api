027ece7cd8ccde4e4ef4ce75e0e3f497053177bc
Niko Marola <niko.marola@businessfinland.fi>
2022-04-06 16:37:18 +0300

    some minor modifications

--- src/api_test.py
+++ src/api_test.py
@@ -7,27 +7,30 @@ import json
 
 from main import JWTGenerator
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 jwt = JWTGenerator(account = env["ACCOUNT"], user = env["S_USER"],\
     private_key_file_path = env["P8_PATH"]+"/rsa_key.p8",\
         private_key_passphrase = env["PRIVATE_KEY_PASSPHRASE"])
 
-jwt = token
-header = {"Authorization": "Bearer " + jwt,\
+jwt_token = jwt.get_token()
+
+header = {"Authorization": "Bearer " + jwt_token,\
         "Content-Type": "application/json",\
             "Accept": "application/json",\
                 "X-Snowflake-Authorization-Token-Type": "KEYPAIR_JWT"}
 
 sql = "select * from testdata" 
-my_data = {"statement": sql, "timeout": 60,\
+
+my_data = {"statement": sql,\
+    "timeout": 30,\
         "resultSetMetaData": {"format": "jsonv2"},\
             "warehouse": env["WAREHOUSE"],\
                 "database": env["DATABASE"],\
                     "schema": env["SCHEMA"]}
  
-url = 'https://'+env["ACCOUNT"]+'/api/statements'
+url = "https://" + env["ACCOUNT"] + "/api/statements"
  
-r = requests.post(url, headers = header, json=my_data)
-json.loads(r.text)
+r = requests.post(url, headers = header, json = my_data)
+r.json()
